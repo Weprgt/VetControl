be6616c43aa58afd22272f0e00649e7c714ca9eb
weprg <weprgt@gmail.com>
2026-09-21 20:15:37 -0600

    Inicializa proyecto VetControl

---
+++ src/main/java/com/mycompany/vetcontrol/VetControl.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vetcontrol;
+
+/**
+ *
+ * @author weprg
+ */
+public class VetControl {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
